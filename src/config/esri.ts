c25e72cde17b99ed8bb7b2e71bc9ecad908fc138
bedzhist <137164516+bedzhist@users.noreply.github.com>
2024-12-26 18:16:15 +0000

    refactor: update project structure and remove unused files

--- src/config/esri.ts
+++ src/config/esri.ts
@@ -1,43 +1,10 @@
-import esriConfig from "@arcgis/core/config";
-import { css, registerStyles } from "@vaadin/vaadin-themable-mixin";
+import esriConfig from '@arcgis/core/config';
+import { registerStyles, unsafeCSS } from '@vaadin/vaadin-themable-mixin';
+import scrollbarStyle from '../styles/scrollbar.scss?inline';
 
 if (import.meta.env.VITE_ESRI_PORTAL_URL) {
   esriConfig.portalUrl = import.meta.env.VITE_ESRI_PORTAL_URL;
 }
 
 // Register custom scrollbar styles for the vaadin-grid component
-registerStyles(
-  "vaadin-grid",
-  css`
-    ::-webkit-scrollbar {
-      width: 6px;
-      height: 6px;
-    }
-
-    ::-webkit-scrollbar-track,
-    ::-webkit-scrollbar-corner {
-      background: rgb(227, 227, 227);
-    }
-
-    .calcite-mode-dark ::-webkit-scrollbar-track,
-    .calcite-mode-dark ::-webkit-scrollbar-corner {
-      background: rgb(24, 24, 24);
-    }
-
-    ::-webkit-scrollbar-thumb:hover {
-      background: rgb(130, 130, 130);
-    }
-
-    .calcite-mode-dark ::-webkit-scrollbar-thumb:hover {
-      background: rgb(227, 227, 227);
-    }
-
-    ::-webkit-scrollbar-thumb {
-      background: rgb(168, 168, 168);
-    }
-
-    .calcite-mode-dark ::-webkit-scrollbar-thumb {
-      background: rgb(118, 118, 118);
-    }
-  `
-);
+registerStyles('vaadin-grid', unsafeCSS(scrollbarStyle));
