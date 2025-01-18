2593e71e76bf8722faf3637825b471cf75c0c6d0
bedzhist <137164516+bedzhist@users.noreply.github.com>
2025-01-18 17:02:34 +0000

    feat: enhance AddData component with living atlas integration and pagination support

--- src/components/AddData/AddData.tsx
+++ src/components/AddData/AddData.tsx
@@ -1,21 +1,31 @@
+import esriConfig from '@arcgis/core/config';
 import Graphic from '@arcgis/core/Graphic';
 import CSVLayer from '@arcgis/core/layers/CSVLayer';
 import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
 import GeoJSONLayer from '@arcgis/core/layers/GeoJSONLayer';
 import Layer from '@arcgis/core/layers/Layer';
 import Field from '@arcgis/core/layers/support/Field';
-import esriRequest from '@arcgis/core/request';
+import PortalItem from '@arcgis/core/portal/PortalItem';
 import {
+  default as esriRequest,
+  default as request
+} from '@arcgis/core/request';
+import {
+  CalciteAvatar,
   CalciteButton,
+  CalciteCard,
+  CalciteCardGroup,
   CalciteInput,
   CalciteLabel,
+  CalcitePagination,
   CalciteTab,
   CalciteTabNav,
   CalciteTabs,
   CalciteTabTitle
 } from '@esri/calcite-components-react';
-import { useActionState, useRef } from 'react';
-import esriConfig from '@arcgis/core/config';
+import { CalcitePaginationCustomEvent } from '@esri/calcite-components';
+import { useActionState, useEffect, useRef, useState } from 'react';
+import { useValue } from '../../hooks';
 
 interface FeatureCollection {
   layers: {
@@ -35,6 +45,15 @@ interface FeatureCollection {
       description: string;
     };
   }[];
+}
+
+interface ResultsItem {
+  id: string;
+  title: string;
+  description: string;
+  type: string;
+  thumbnail: string;
+  owner: string;
 }
 
 export interface AddDataProps {
@@ -57,6 +76,7 @@ const buildLayersFromCollection = (
     const fields = layer.layerDefinition.fields.map((field) => {
       return Field.fromJSON(field);
     });
+    // TODO: Handle renderer
     /* const renderer = layer.layerDefinition.drawingInfo.renderer;
     switch (renderer.symbol.type) {
       case 'esriSMS':
@@ -140,9 +160,14 @@ const readFileAsText = (file: File): Promise<string> => {
 };
 
 const GENERATE_SERVICE_URL = `${esriConfig.portalUrl}/sharing/rest/content/features/generate`;
+const COMMUNITY_GROUPS_SERVICE_URL = `${esriConfig.portalUrl}/sharing/rest/community/groups`;
+const CONTENT_GROUPS_SERVICE_URL = `${esriConfig.portalUrl}/sharing/rest/content/groups`;
+const RESULTS_PAGE_SIZE = 10;
 
 export function AddData(props: AddDataProps) {
   const inputFileRef = useRef<HTMLInputElement>(null);
+
+  const livingAtlasGroupId = useValue<string>('');
 
   const [, fileFormAction, isFileFormLoading] = useActionState<null, FormData>(
     async () => {
@@ -167,8 +192,6 @@ export function AddData(props: AddDataProps) {
             url: URL.createObjectURL(file)
           });
           view.map.add(layer);
-          await view.whenLayerView(layer);
-          view.goTo(layer.fullExtent);
           break;
         }
         case 'application/zip': {
@@ -197,10 +220,8 @@ export function AddData(props: AddDataProps) {
           });
           const featureCollection: FeatureCollection =
             response.data.featureCollection;
-          const { layers, allGraphics } =
-            buildLayersFromCollection(featureCollection);
+          const { layers } = buildLayersFromCollection(featureCollection);
           view.map.addMany(layers);
-          view.goTo(allGraphics);
           break;
         }
         case 'application/geo+json': {
@@ -208,8 +229,6 @@ export function AddData(props: AddDataProps) {
             url: URL.createObjectURL(file)
           });
           view.map.add(layer);
-          await view.whenLayerView(layer);
-          view.goTo(layer.fullExtent);
           break;
         }
         case 'application/vnd.google-earth.kml+xml': {
@@ -224,10 +243,8 @@ export function AddData(props: AddDataProps) {
           });
           const featureCollection: FeatureCollection =
             response.data.featureCollection;
-          const { layers, allGraphics } =
-            buildLayersFromCollection(featureCollection);
+          const { layers } = buildLayersFromCollection(featureCollection);
           view.map.addMany(layers);
-          view.goTo(allGraphics);
           break;
         }
         case 'application/vnd.google-earth.kmz': {
@@ -261,10 +278,8 @@ export function AddData(props: AddDataProps) {
             });
             const featureCollection: FeatureCollection =
               response.data.featureCollection;
-            const { layers, allGraphics } =
-              buildLayersFromCollection(featureCollection);
+            const { layers } = buildLayersFromCollection(featureCollection);
             view.map.addMany(layers);
-            view.goTo(allGraphics);
           }
           break;
         }
@@ -291,22 +306,169 @@ export function AddData(props: AddDataProps) {
       }
       const layer = await Layer.fromArcGISServerUrl({ url });
       view.map.add(layer);
-      await view.whenLayerView(layer);
-      view.goTo(layer.fullExtent);
       return null;
     },
     null
   );
+  const [resultItems, setResultItems] = useState<ResultsItem[]>();
+  const [resultsTotal, setResultsTotal] = useState<number>();
+  const [resultsStart, setResultsStart] = useState<number>(1);
+
+  const fetchLivingAtlasResults = async (
+    livingAtlasGroupId: string,
+    start?: number,
+    signal?: AbortSignal
+  ) => {
+    const query = {
+      num: RESULTS_PAGE_SIZE,
+      start,
+      sortField: 'modified',
+      sortOrder: 'desc',
+      filter:
+        '(type:"Map Service" OR type:"Image Service" OR type:"Feature Service" OR type:"Vector Tile Service" OR type:"OGCFeatureServer" OR type:"WMS" OR type:"WFS" OR type:"WMTS" OR type:"KML" OR type: "Stream Service" OR type: "Feed" OR type:"Media Layer" OR type:"Group Layer" OR type:"GeoJson" OR type:"Knowledge Graph Service" OR type:"Knowledge Graph Layer" OR (type: "Feature Service" AND typekeywords: "OrientedImageryLayer") OR (type: "Feature Service" AND typekeywords: "CatalogLayer") OR (type:"Feature Collection" AND typekeywords:"Route Layer") OR (type:"Feature collection" AND typekeywords:"Markup")) -typekeywords: "Table"',
+      enriched: true,
+      q: '(-typekeywords:"Elevation 3D Layer" AND -typekeywords:"IndoorPositioningDataService") -typekeywords:("MapAreaPackage") -type:("Map Area" OR "Indoors Map Configuration" OR "Code Attachment")',
+      displaySublayers: true,
+      displayHighlights: true,
+      displayServiceProperties: true,
+      f: 'json'
+    };
+    const contentGroupsSearchUrl = `${CONTENT_GROUPS_SERVICE_URL}/${livingAtlasGroupId}/search`;
+    const response = await esriRequest(contentGroupsSearchUrl, {
+      query,
+      signal
+    });
+    const data = response.data;
+    const results = data.results;
+    setResultItems(results);
+    setResultsTotal(data.total);
+  };
+  const addLayerFromPortalClick = async (
+    resultItem: ResultsItem,
+    event: React.MouseEvent<HTMLCalciteButtonElement, MouseEvent>
+  ) => {
+    const view = props.view;
+    if (!view) {
+      // Handle error
+      return;
+    }
+    const target = event.currentTarget;
+    const portalItem = new PortalItem({
+      id: resultItem.id
+    });
+    target.loading = true;
+    target.disabled = true;
+    const layer = await Layer.fromPortalItem({
+      portalItem
+    });
+    target.loading = false;
+    target.disabled = false;
+    view.map.add(layer);
+  };
+  const handleResultsPaginationChange = (
+    event: CalcitePaginationCustomEvent<void>
+  ) => {
+    const currLivingAtlasGroupId = livingAtlasGroupId.current;
+    if (!currLivingAtlasGroupId) {
+      // Handle error
+      return;
+    }
+    const newStartItem = event.target.startItem;
+    setResultsStart(newStartItem);
+    fetchLivingAtlasResults(currLivingAtlasGroupId, newStartItem);
+  };
+
+  useEffect(() => {
+    const init = async (signal: AbortSignal) => {
+      const communityGroupsQuery = {
+        q: 'title:"LAW Search" AND owner:Esri_LivingAtlas',
+        num: 1,
+        sortField: 'title',
+        sortOrder: 'asc',
+        f: 'json'
+      };
+      const communityGroupsResponse = await request(
+        COMMUNITY_GROUPS_SERVICE_URL,
+        {
+          query: communityGroupsQuery,
+          signal
+        }
+      );
+      const newLivingAtlasGroupId = communityGroupsResponse.data.results[0].id;
+      livingAtlasGroupId.current = newLivingAtlasGroupId;
+      fetchLivingAtlasResults(newLivingAtlasGroupId, resultsStart, signal);
+    };
+    const abortController = new AbortController();
+    init(abortController.signal);
+    return () => {
+      abortController.abort();
+    };
+  }, []);
 
   return (
     <CalciteTabs
       layout="center"
-      className="p-3"
+      className="h-100"
     >
       <CalciteTabNav slot="title-group">
+        <CalciteTabTitle>Portal</CalciteTabTitle>
         <CalciteTabTitle>File</CalciteTabTitle>
         <CalciteTabTitle>URL</CalciteTabTitle>
       </CalciteTabNav>
+      <CalciteTab>
+        <div className="d-flex flex-column h-100">
+          <CalciteCardGroup
+            label="Content Items"
+            className="overflow-auto h-100"
+          >
+            {resultItems?.map((resultItem) => (
+              <CalciteCard
+                key={resultItem.id}
+                thumbnailPosition="inline-end"
+                className="w-100"
+              >
+                <img
+                  slot="thumbnail"
+                  alt="Sample image alt"
+                  src={`${esriConfig.portalUrl}/sharing/rest/content/items/${resultItem.id}/info/${
+                    resultItem.thumbnail
+                  }`}
+                />
+                <span slot="heading">{resultItem.title}</span>
+                <div
+                  slot="footer-start"
+                  className="overflow-hidden"
+                >
+                  <CalciteAvatar
+                    scale="s"
+                    thumbnail={`${esriConfig.portalUrl}/sharing/rest/community/users/${
+                      resultItem.owner
+                    }/info/blob.png`}
+                  />
+                  <span className="text-truncate ms-3">{resultItem.owner}</span>
+                </div>
+                <CalciteButton
+                  slot="footer-end"
+                  iconStart="plus"
+                  appearance="outline"
+                  kind="neutral"
+                  scale="s"
+                  onClick={(e) => addLayerFromPortalClick(resultItem, e)}
+                >
+                  Add
+                </CalciteButton>
+              </CalciteCard>
+            ))}
+          </CalciteCardGroup>
+          <CalcitePagination
+            pageSize={RESULTS_PAGE_SIZE}
+            startItem={resultsStart}
+            totalItems={resultsTotal}
+            className="justify-center py-3 border-t-1 border-color-1"
+            onCalcitePaginationChange={handleResultsPaginationChange}
+          />
+        </div>
+      </CalciteTab>
       <CalciteTab>
         <form
           className="p-7"
