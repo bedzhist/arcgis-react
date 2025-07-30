1fe2caa1205a830e19af60fec6f4929d16b8f868
Marco Pasqualetti <marco.pasqualetti25@gmail.com>
2025-07-30 11:13:10 +0200

    refactor: remove unused event-related types from index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,3 @@
-import { EventName } from '@lit/react';
-
 export type ArcGISLayer =
   | __esri.Layer
   | __esri.Sublayer
@@ -22,17 +20,3 @@ export type AllArcGISLayer =
   | __esri.StreamLayer
   | __esri.GeoJSONLayer
   | __esri.KMLLayer;
-
-export type EventNames = Record<string, EventName | string>;
-export type ElementProps<I> = Partial<Omit<I, keyof HTMLElement>>;
-export type EventListeners<R extends EventNames> = {
-  [K in keyof R]?: R[K] extends EventName
-    ? (e: R[K]['__eventType']) => void
-    : (e: Event) => void;
-};
-export type ComponentProps<
-  I,
-  E extends EventNames = Record<string, EventName | string>
-> = Omit<React.HTMLAttributes<I>, keyof E | keyof ElementProps<I>> &
-  EventListeners<E> &
-  ElementProps<I>;
