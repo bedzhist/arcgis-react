be2b8ede34fb72dcdc50cbed283349ab782eba82
Marco Pasqualetti <marco.pasqualetti25@gmail.com>
2025-08-11 19:45:42 +0200

    feat: rename `config` directory to `lib`

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './config';
+import './lib/index.ts';
 import { AuthProvider, ThemeProvider } from './contexts';
 import './styles/index.css';
 
